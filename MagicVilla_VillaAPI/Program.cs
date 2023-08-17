ed87c7397ca3103fb4e4264a18df3a0d18a64cb5
Abdulkabir Abdulwasiu <aabdulwasiu@vatebra.com>
2023-08-17 12:59:26 +0100

    Commit all changes

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,8 +1,22 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//Take note to be able to set the request to json use addNewTonsoftJson while for xml use the other one
+//builder.Services.AddControllers(option =>
+//{
+//    option.ReturnHttpNotAcceptable = true;
+//}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
+
+//using serilog for logger
+Log.Logger = new LoggerConfiguration().MinimumLevel.Information()
+    .WriteTo.File("log/villaLogs.txt", rollingInterval: RollingInterval.Day).CreateLogger();
+
+builder.Host.UseSerilog();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
